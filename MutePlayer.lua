b2391a50cf1a5d08586bd046a5abd1d76974f97d
Randactyl <rafowler@randactyl.com>
2015-08-26 20:08:23 -0700

    add preliminary scroll list
    doesn't really do anything yet. It just lists the players in the mute
    list

--- MutePlayer.lua
+++ MutePlayer.lua
@@ -1,4 +1,5 @@
 local libChat = LibStub("libChat-1.0")
+local LibScroll = LibStub("LibScroll")
 local savedVars = nil
 
 local function AddMute(playerName)
@@ -36,6 +37,45 @@ local function InitializeSlashCommands()
 	end
 end
 
+local function CreateScrollList()
+	local function SortScrollList(objA, objB)
+		return objA.data.name < objB.data.name
+	end
+	local function OnRowSelect(previouslySelectedData, selectedData, reselectingDuringRebuild)
+		if not selectedData then return end
+		d("you selected a row!")
+	end
+	local function setupDataRow(rowControl, data, scrollList)
+		rowControl:SetText(data.name)
+		rowControl:SetFont("ZoFontWinH4")
+
+		rowControl:SetHandler("OnMouseUp", function()
+			ZO_ScrollList_MouseClick(scrollList, rowControl)
+		end)
+	end
+
+	local scrollData = {
+		name = "MutePlayerScrollList",
+		parent = MutePlayerList,
+		width = 930,
+		height = 690,
+
+		rowHeight = 30,
+		setupCallback = setupDataRow,
+		selectCallback = OnRowSelect,
+		dataTypeSelectSound = SOUNDS.BOOK_CLOSE,
+		sortFunction = SortScrollList,
+	}
+	local scrollList = LibScroll:CreateScrollList(scrollData)
+	scrollList:SetAnchor(TOPLEFT, MutePlayerList, TOPLEFT, 50, 50)
+
+	local dataItems = {}
+	for playerName, _ in pairs(savedVars.muted) do
+		table.insert(dataItems, {name = playerName,})
+	end
+	scrollList:Update(dataItems)
+end
+
 local function OnAddonLoaded(eventCode, addonName)
 	if addonName ~= "MutePlayer" then return end
 	EVENT_MANAGER:UnregisterForEvent("MutePlayer_OnAddonLoaded", EVENT_ADD_ON_LOADED)
@@ -43,6 +83,8 @@ local function OnAddonLoaded(eventCode, addonName)
 	savedVars = ZO_SavedVars:NewAccountWide("MutePlayerSavedVariables", 1, nil, { muted = {}, })
 
 	libChat:registerName(modifySenderName, "MutePlayer")
+
+	CreateScrollList()
 
 	InitializeSlashCommands()
 end
@@ -58,7 +100,7 @@ function CHAT_SYSTEM:ShowPlayerContextMenu(playerName, rawName)
     local otherPlayerIsInPlayersGroup = not otherPlayerIsDecoratedName and IsPlayerInGroup(rawName)
 
     if not localPlayerIsGrouped or (localPlayerIsGroupLeader and not otherPlayerIsInPlayersGroup) then
-        AddMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_ADD_GROUP), function() 
+        AddMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_ADD_GROUP), function()
             local SENT_FROM_CHAT = false
             local DISPLAY_INVITED_MESSAGE = true
             TryGroupInviteByName(playerName, SENT_FROM_CHAT, DISPLAY_INVITED_MESSAGE) end)
