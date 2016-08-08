06486f93c66a77720957066d6b77309019140a51
Randactyl <rafowler@randactyl.com>
2016-08-08 09:50:00 -0700

    remove scrolllist stuff

--- MutePlayer.lua
+++ MutePlayer.lua
@@ -1,6 +1,5 @@
 --do keyword matching
 local libChat = LibStub("libChat-1.0")
-local LibScroll = LibStub("LibScroll")
 local savedVars = nil
 
 local function AddMute(playerName)
@@ -38,45 +37,6 @@ local function InitializeSlashCommands()
 	end
 end
 
-local function CreateScrollList()
-	local function SortScrollList(objA, objB)
-		return objA.data.name < objB.data.name
-	end
-	local function OnRowSelect(previouslySelectedData, selectedData, reselectingDuringRebuild)
-		if not selectedData then return end
-		d("you selected a row!")
-	end
-	local function setupDataRow(rowControl, data, scrollList)
-		rowControl:SetText(data.name)
-		rowControl:SetFont("ZoFontWinH4")
-
-		rowControl:SetHandler("OnMouseUp", function()
-			ZO_ScrollList_MouseClick(scrollList, rowControl)
-		end)
-	end
-
-	local scrollData = {
-		name = "MutePlayerScrollList",
-		parent = MutePlayerList,
-		width = 930,
-		height = 690,
-
-		rowHeight = 30,
-		setupCallback = setupDataRow,
-		selectCallback = OnRowSelect,
-		dataTypeSelectSound = SOUNDS.BOOK_CLOSE,
-		sortFunction = SortScrollList,
-	}
-	local scrollList = LibScroll:CreateScrollList(scrollData)
-	scrollList:SetAnchor(TOPLEFT, MutePlayerList, TOPLEFT, 50, 50)
-
-	local dataItems = {}
-	for playerName, _ in pairs(savedVars.muted) do
-		table.insert(dataItems, {name = playerName,})
-	end
-	scrollList:Update(dataItems)
-end
-
 local function OnAddonLoaded(eventCode, addonName)
 	if addonName ~= "MutePlayer" then return end
 	EVENT_MANAGER:UnregisterForEvent("MutePlayer_OnAddonLoaded", EVENT_ADD_ON_LOADED)
@@ -84,8 +44,6 @@ local function OnAddonLoaded(eventCode, addonName)
 	savedVars = ZO_SavedVars:NewAccountWide("MutePlayerSavedVariables", 1, nil, { muted = {}, })
 
 	libChat:registerName(modifySenderName, "MutePlayer")
-
-	--CreateScrollList()
 
 	InitializeSlashCommands()
 end
