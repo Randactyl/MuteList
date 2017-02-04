358bf69085ff9e7a26bb2d3ddc2dfc987c03d519
Randactyl <rafowler@randactyl.com>
2017-02-03 22:56:00 -0800

    add LibCustomMenu version 4.2

--- util.lua
+++ util.lua
@@ -16,41 +16,41 @@ function CHAT_SYSTEM:ShowPlayerContextMenu(playerName, rawName)
     local otherPlayerIsInPlayersGroup = not otherPlayerIsDecoratedName and IsPlayerInGroup(rawName)
 
     if not localPlayerIsGrouped or (localPlayerIsGroupLeader and not otherPlayerIsInPlayersGroup) then
-        AddMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_ADD_GROUP), function()
+        AddCustomMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_ADD_GROUP), function()
             local SENT_FROM_CHAT = false
             local DISPLAY_INVITED_MESSAGE = true
             TryGroupInviteByName(playerName, SENT_FROM_CHAT, DISPLAY_INVITED_MESSAGE)
         end)
     elseif otherPlayerIsInPlayersGroup and localPlayerIsGroupLeader then
-        AddMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_REMOVE_GROUP), function()
+        AddCustomMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_REMOVE_GROUP), function()
             GroupKickByName(rawName)
         end)
     end
 
-    AddMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_WHISPER), function()
+    AddCustomMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_WHISPER), function()
         self:StartTextEntry(nil, CHAT_CHANNEL_WHISPER, playerName)
     end)
 
     if(not IsIgnored(rawName)) then
-        AddMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_ADD_IGNORE), function()
+        AddCustomMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_ADD_IGNORE), function()
             AddIgnore(playerName)
         end)
     end
 
     if not settings.IsMuted(playerName) then
-        AddMenuItem(GetString(SI_MUTELIST_MUTE_OPTION), function()
+        AddCustomMenuItem(GetString(SI_MUTELIST_MUTE_OPTION), function()
             settings.Mute(playerName)
             df(GetString(SI_MUTELIST_MUTE_MESSAGE), playerName)
         end)
     end
 
     if(not IsFriend(rawName)) then
-        AddMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_ADD_FRIEND), function()
+        AddCustomMenuItem(GetString(SI_CHAT_PLAYER_CONTEXT_ADD_FRIEND), function()
             ZO_Dialogs_ShowDialog("REQUEST_FRIEND", {name = rawName})
         end)
     end
 
-    AddMenuItem(zo_strformat(SI_CHAT_PLAYER_CONTEXT_REPORT, rawName), function()
+    AddCustomMenuItem(zo_strformat(SI_CHAT_PLAYER_CONTEXT_REPORT, rawName), function()
         ZO_ReportPlayerDialog_Show(playerName, REPORT_PLAYER_REASON_CHAT_SPAM, rawName)
     end)
 
